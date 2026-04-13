e36eb4af7ff8c59f2aec8dac6624e83a54159232
Anil Patil <anilpatil4113@gmail.com>
2026-04-13 23:53:40 +0530

    cors frontend connectivity issue

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -157,6 +157,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'http://localhost:3000',
+    'https://ai-career-copilot-phi.vercel.app'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
